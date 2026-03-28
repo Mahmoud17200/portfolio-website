d3728fdc0fbdf9eaa38260b8044f264e023f1d41
mahmoud <mahmoudlodzz12@gmail.com>
2026-03-28 17:52:09 +0200

    update

--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -5,6 +5,8 @@ import projects from "../../data/projects-api";
 import "/src/pages-css/projectsPage.css";
 
 function ProjectsPage() {
+  const [activeId, setActiveId] = useState(null);
+
   return (
     <div className="projects-page">
       <div className="projects">
@@ -26,21 +28,23 @@ function ProjectsPage() {
           <div className="container-fluid px-0">
             <div className="row g-0">
               {projects.map((project) => {
-                const [preview, setPreview] = useState(false);
-
                 return (
                   <div
                     key={project.id}
                     className="m-auto mb-lg-4 mb-lg-3 mb-sm-2 col-lg-8 col-md-8 col-sm-12"
                   >
                     <div
-                      className={`project-card wow animate__animated animate__fadeInDown ${preview ? "active-preview" : ""}`}
+                      className={`project-card ${activeId === project.id ? "active-preview" : ""}`}
                     >
                       <div className="card-body">
                         <div className="top">
                           <h3>{project.title}</h3>
                           <span
-                            onClick={() => setPreview(!preview)}
+                            onClick={() =>
+                              setActiveId(
+                                activeId === project.id ? null : project.id,
+                              )
+                            }
                             className="quick-preview d-flex"
                           >
                             <img src="/icons/tap-icon.png" alt="" />
@@ -72,7 +76,7 @@ function ProjectsPage() {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            class="bi bi-github"
+                            className="bi bi-github"
                             viewBox="0 0 16 16"
                           >
                             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
@@ -89,7 +93,7 @@ function ProjectsPage() {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            class="bi bi-box-arrow-up-right"
+                            className="bi bi-box-arrow-up-right"
                             viewBox="0 0 16 16"
                           >
                             <path
